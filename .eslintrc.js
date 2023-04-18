711d4369996762c5bca1b6b6dc88f3fed607d4f0
Valery <quaizzz@mail.ru>
2023-04-18 15:57:12 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+    root: true,
+    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/eslint-config-prettier"],
+    rules: {
+        indent: ["error", 4, { SwitchCase: 1 }],
+        "prettier/prettier": [
+            "error",
+            {
+                tabWidth: 4,
+                endOfLine: "auto",
+                code: 120,
+                printWidth: 120,
+            },
+        ],
+    },
+};
---
+++ src/components/map/MapBox.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="map" style="width: 100%; height: 100%"></div>
+</template>
+
+<script>
+import { initMap } from "./map";
+export default {
+    mounted() {
+        initMap();
+    },
+    data() {
+        return {};
+    },
+};
+</script>
+<style>
+.mapboxgl-popup-content {
+    width: 250px;
+}
+.mapboxgl-marker {
+    cursor: pointer;
+}
+</style>
